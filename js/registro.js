8a674126ba3d729c51ed539ed48c6e8f0b29ed6d
tl4yud0 <tl4yud0@gmail.com>
2020-07-08 19:26:22 -0400

    final

--- js/registro.js
+++ js/registro.js
@@ -1,14 +1,11 @@
 $(document).ready(function(){
    $('#contactform').submit(function(event){
-      alert("medio clic")
 		event.preventDefault();
       $.ajax({
 			type: 'POST',
          data: $(this).serialize(),
-         url: "./registro.php",
+         url: "./register.php",
          success: function(data) {
-            alert('AJAX call was successful!');
-            alert('Data from the server' + data);
             $('#contactform').css('display',' none');
             $('.thanks').css('display','block');
           },    
--- registro.php
+++ registro.php
@@ -6,7 +6,7 @@ $subs_email = utf8_decode($_POST['email']);
 $subs_option = utf8_decode($_POST['option']);
 $subs_phone = utf8_decode($_POST['phone']);
 
-$texto = "*Name: ".$subs_name." *Email: ".$subs_email." *Option: ".$subs_option." *Phone: ".$subs_phone;
+$texto = "*Nombre de colaborador: ".$subs_name."    *Correo electronico: ".$subs_email."   *Tipo de colaboracion: ".$subs_option."    *Telefono: ".$subs_phone;
 
 $file = fopen("miarchivo.txt", "a");
 fwrite($file, $texto. PHP_EOL);
@@ -14,13 +14,27 @@ fclose($file);
 
 $from = "test@circa.com";
 
-$to = $subs_email;
-$subject = "Saludos de Circa ";
-$message = "Hola ".$subs_name." nos pondremos en contacto contigo para mas detalles :)";
-$headers = "From:" . $from;
-mail($to,$subject,$message, $headers);
-echo "The email message was sent.";
+//$to = $subs_email;
+//$subject = "Saludos de Circa ";
+//$message = "Hola ".$subs_name." nos pondremos en contacto contigo para mas detalles :)";
+//$headers = "From:" . $from;
+//$email = mail($to,$subject,$message, $headers);
+//var_dump($email);exit(); 
+//echo "The email message was sent.";
 
+$to      = 'sparck_me@hotmail.com';
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+$msg = 'From:'.$subs_name."\n";
+$msg.= $texto;
+$subject = 'Email contact';
+if(mail($to, $subject, $content, $headers)){
+    die('done');
+}else{
+    echo "<i class='fa fa-exclamation-triangle'></i> <span>Send message error.</span>";
+    die;
+}
 
 
 ?>
